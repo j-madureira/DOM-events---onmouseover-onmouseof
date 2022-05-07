91f00a4aec967c3aee2176d651ca215f43a9e573
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-05-07 16:03:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mudarTexto(){
+     var p1 = document.getElementById("p1")
+     p1.innerHTML = "<p>Mudado ao passar por cima</p>";
+}
+
+function voltarTexto(){
+    var p1 = document.getElementById("p1");
+    p1.innerHTML = "<p>Texto</p>";
+}
